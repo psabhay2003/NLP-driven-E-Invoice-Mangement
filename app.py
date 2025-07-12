b269980076d416c681a8832d2f22500b5b0128f6
Abhay Pratap Singh <psabhay2003@gmail.com>
2025-07-12 15:59:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,5 +85,9 @@ def append_to_existing_csv():
     flash("✅ Appended to invoices.csv successfully!")
     return send_file(existing_csv_path, as_attachment=True)
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
