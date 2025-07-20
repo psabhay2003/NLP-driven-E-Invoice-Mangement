7176ff7baa9ebbe9124ff7961f9f4b1c15d4f180
Abhay Pratap Singh <psabhay2003@gmail.com>
2025-07-20 23:27:40 +0530

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -1,7 +1,7 @@
 from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 # Replace this with your actual Hugging Face model repo
-HF_MODEL_NAME = psabhay2003/t5_invoice_model"  # e.g., "google/flan-t5-small"
+HF_MODEL_NAME = "psabhay2003/t5_invoice_model"  # e.g., "google/flan-t5-small"
 
 def load_model_and_tokenizer():
     print(f"📦 Loading model from Hugging Face: {HF_MODEL_NAME}")
